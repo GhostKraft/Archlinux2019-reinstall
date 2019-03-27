268e9a9e6c9270dfc404e7e70fefbeac92fe2d36
GhostKraft <48885256+GhostKraft@users.noreply.github.com>
2019-03-27 11:59:11 +0300

    Update arch-reinstall2.sh

--- arch-reinstall2.sh
+++ arch-reinstall2.sh
@@ -50,16 +50,8 @@ echo '[multilib]' >> /etc/pacman.conf
 echo 'Include = /etc/pacman.d/mirrorlist' >> /etc/pacman.conf
 pacman -Syy
 
-echo "Куда устанавливем Arch Linux на виртуальную машину?"
-read -p "1 - Да, 0 - Нет: " vm_setting
-if [[ $vm_setting == 0 ]]; then
-  gui_install="xorg-server xorg-drivers xorg-xinit"
-elif [[ $vm_setting == 1 ]]; then
-  gui_install="xorg-server xorg-drivers xorg-xinit virtualbox-guest-utils"
-fi
-
 echo 'Ставим иксы и драйвера'
-pacman -S $gui_install
+pacman -S xorg-server xorg-drivers xorg-xinit
 
 echo 'Ставим Xfce, lightdm'
 pacman -S xfce4 xfce4-goodies lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings --noconfirm
