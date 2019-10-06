ca2fd24ff4816b9ddf39afc846606a2eb58dd798
GhostKraft <48885256+GhostKraft@users.noreply.github.com>
2019-10-06 21:05:36 +0300

    Update arch-reinstall1.sh

--- arch-reinstall1.sh
+++ arch-reinstall1.sh
@@ -30,7 +30,7 @@ echo '3.1 Выбор зеркал для загрузки. Ставим зерк
 echo "Server = https://mirror.yandex.ru/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
 
 echo '3.2 Установка основных пакетов'
-pacstrap /mnt base base-devel
+pacstrap /mnt base base-devel linux
 
 echo '3.3 Настройка системы, создание FSTAB'
 genfstab -U /mnt >> /mnt/etc/fstab
