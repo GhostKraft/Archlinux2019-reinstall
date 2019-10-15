5d3ea94f01aacc50329e09e763c43184edf07750
GhostKraft <48885256+GhostKraft@users.noreply.github.com>
2019-10-15 12:33:21 +0300

    Update arch-reinstall2.sh

--- arch-reinstall2.sh
+++ arch-reinstall2.sh
@@ -62,7 +62,7 @@ sudo pacman -S bspwm sxhkd dmenu --noconfirm
 mkdir -p /home/$hostname/.config/sxhkd
 cp /usr/share/doc/bspwm/examples/bspwmrc /home/$hostname/.config/bspwm/
 cp /usr/share/doc/bspwm/examples/sxhkdrc /home/$hostname/.config/sxhkd/
-chmod x home/$hostname/.config/bspwm/bspwmrc
+chmod x /home/$hostname/.config/bspwm/bspwmrc
 
 echo 'Ставим Дополнительное ПО'
 sudo pacman -S pcmanfm rofi lxappearance nitrogen polkit-gnome lxtask gparted hardinfo neofetch xorg-xrandr rxvt-unicode urxvt-perls dunst viewnior flameshot sakura leafpad xorg-xev ntfs-3g bash-completion qt5-styleplugins qt5ct mpd mpc ncmpcpp slop xorg-xsetroot firefox firefox-i18n-ru gsimplecal --noconfirm 
