81591db2aa22311264f6834ff9f5df46810a9457
GhostKraft <48885256+GhostKraft@users.noreply.github.com>
2019-10-18 04:20:59 +0300

    Update arch-reinstall2.sh

--- arch-reinstall2.sh
+++ arch-reinstall2.sh
@@ -7,6 +7,7 @@ echo $hostname > /etc/hostname
 
 echo 'Выбираем часовой пояс'
 ln -svf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
+hwclock --systohc
 
 echo '3.4 Добавляем русскую локаль системы'
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
